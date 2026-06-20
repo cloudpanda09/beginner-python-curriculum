0736c99574ffd77b0ab8eef479b4f55d418d7bad
cloudpanda09 <cloudpanda09@gmail.com>
2026-06-20 00:29:35 +0000

    Finished HW 2

--- lesson2/homework/homework.py
+++ lesson2/homework/homework.py
@@ -12,19 +12,29 @@ print("Remainder: ", n1%n2)
 # Ask the user for their favorite animal and favorite color.
 # Print a sentence combining them like: "A blue tiger would be awesome!"
 
+favorite_animal = input("What is your favorite animal?")
+favorite_color = input("What is your favorite color?")
 
+print("A " + favorite_color + " " + favorite_animal + " would be awesome!")
 
 # Homework Problem 3
 # Use a for loop to print all the even numbers from 0 to 10 (including 10).
 
-
+for i in range(11):
+    print(i)
 
 # Homework Problem 4
 # Ask the user how many push-ups they can do.
 # Multiply it by 7 and print how many they could do in a week.
 
+pushups = int(input("How many pushups can you do?"))
+weekly_pushups = pushups * 7
 
+print("You could do " + str(weekly_pushups) + " pushups in a week!")
 
 # Homework Problem 5
 # Use a for loop to print the square of each number from 1 to 6.
 # (Example: 1*1=1, 2*2=4, etc.)
+
+for i in range(1, 6):
+    print(i**2)
